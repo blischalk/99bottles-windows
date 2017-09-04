99b149c0b0de95f1afe6df196482779ab8d53d11
Brett Lischalk <blischalk@opinionlab.com>
2017-09-03 22:39:59 -0500

    Fix formatting

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -16,24 +16,24 @@ int main(int argc, char **argv) {
 	for(i=99; i>0; i--) {
 		if (i > 1) {
 			printf("%i bottles of beer on the wall\n", i);
-		    printf("%i bottles of beer...\n", i);
+			printf("%i bottles of beer...\n", i);
 		} else {
 			printf("%i bottle of beer on the wall\n", i);
-		    printf("%i bottle of beer...\n", i);
+			printf("%i bottle of beer...\n", i);
 		}
 
 		printf("You take one down, pass it around...\n");
 
 		switch(i) {
-			case 2:
-				printf("%i bottle of beer on the wall\n", (i-1));
-				break;
-		    case 1:
-				printf("No more bottles of beer on the wall!\n");
-				break;
-			default:
-				printf("%i bottles of beer on the wall\n", i);
-				break;
+		case 2:
+			printf("%i bottle of beer on the wall\n", (i-1));
+			break;
+		case 1:
+			printf("No more bottles of beer on the wall!\n");
+			break;
+		default:
+			printf("%i bottles of beer on the wall\n", i);
+			break;
 		};
 
 		// Place some spaces in between each phrase
